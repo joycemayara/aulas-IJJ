3b7296f2be2b5acdd4de47a02dda7e9adfbbbfb6
joycemayara <joyceemayaraalmeida@gmail.com>
2024-06-17 19:09:53 -0300

    Captura de dados e cálculo da nota

---
+++ 13-06.py
@@ -0,0 +1,15 @@
+# Obtenção de dados do usuário
+nome_do_usuario = input("Digite o nome do usuário: ")
+altura_do_usuario = input("Insira a altura do usuário em centímetros: ")
+idade_do_usuario = int(input("Insira a idade do usuário: "))
+
+# Obtenção do valor das notas
+primeira_nota = float(input("Digite a primeira nota do usuário: "))
+segunda_nota = float(input("Digite a segunda nota do usuário: "))
+
+
+# Cálculo da soma das notas 
+soma_das_notas = primeira_nota + segunda_nota
+
+# Exibição do resultado da soma das notas
+print("Soma das notas: ", soma_das_notas)
